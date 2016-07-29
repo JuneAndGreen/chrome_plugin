f4f00c43a33c93476c8b2f99d72bd747eade483a
hzxiejin <hzxiejin@corp.netease.com>
2016-07-29 15:42:29 +0800

    调整css为less

--- engcard/gulpfile.js
+++ engcard/gulpfile.js
@@ -2,6 +2,7 @@
 
 const gulp  = require('gulp');
 const react = require('gulp-react');
+const less = require('gulp-less');
 
 function jsx2js(filename) {
 	return gulp.src(filename)
@@ -9,12 +10,41 @@ function jsx2js(filename) {
     .pipe(gulp.dest('./engcard/background/build/'));
 }
 
-gulp.task('init', () => {
+function insertLess2css() {
+	return gulp.src('./less/insert.less')
+		.pipe(less())
+		.pipe(gulp.dest('./src/'));
+}
+
+function indexLess2css() {
+	return gulp.src('./less/index.less')
+		.pipe(less())
+		.pipe(gulp.dest('./src/background/res/'));
+}
+
+// 初始化
+gulp.task('initJsx', () => {
 	return jsx2js('./jsx/*.jsx');
 });
 
-gulp.task('jsx2js', ['init'], function() {
-	return gulp.watch('./jsx/*.jsx', (event) => {
-		jsx2js(event.path);
+gulp.task('initInsertLess', () => {
+	return insertLess2css();
+});
+
+gulp.task('initIndexLess', () => {
+	return indexLess2css();
+});
+
+// 监听
+
+gulp.task('default', ['initJsx', 'initInsertLess', 'initIndexLess'], function() {
+	return gulp.watch(['./jsx/*.jsx', './less/insert.less', './less/index.less'], (event) => {
+		if(event.path.indexOf('insert.less') !== -1) {
+			return insertLess2css();
+		} else if(event.path.indexOf('index.less') !== -1) {
+			return indexLess2css();
+		} else {
+			return jsx2js(event.path);
+		}
   });
 });
