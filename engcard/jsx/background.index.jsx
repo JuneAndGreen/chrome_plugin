b77ea37997cac36b7979e4089f03d45990b2cb01
hzxiejin <hzxiejin@corp.netease.com>
2016-08-03 18:45:26 +0800

    完善encard代码

--- engcard/jsx/background.index.jsx
+++ engcard/jsx/background.index.jsx
@@ -1,12 +1,130 @@
+'use strict';
+
+const hists = [];
+const handels = {};
+
+/*var data = {
+  'query': 'test',
+  'translation': [
+    '今天天气不错',
+    '今天天气不错',
+    '今天天气不错',
+    '今天天气不错',
+    '今天天气不错',
+    '今天天气不错',
+    '今天天气不错',
+    '今天天气不错',
+    '今天天气不错'
+  ],
+  'basic': {
+    'phonetic': 'yahei, xx',
+    'uk-phonetic': 'sss',
+    'us-phonetic': 'fff',
+    'explains': [
+      'cc',
+      'ss',
+      'gg'
+    ]
+  },
+  'web': [
+    {key: 'xxx', value: ['tttt']},
+    {key: 'xx阿斯x', value: ['ttt阿斯达斯的t']},
+    {key: 'xxx', value: ['tttt', 'xxxxx']}
+  ],
+  'hists': [
+    'xxxxx',
+    'xxxxx',
+    'xxxxx',
+    'xxxxx'
+  ]
+};*/
+
+/**
+ * 封装jsonp请求
+ */
+function send(text, callback) {
+  if(!text.trim()) return;
+
+  // 压入历史记录
+  addHist(text);
+
+  text = encodeURIComponent(text);
+
+  let script = document.createElement('script');
+  script.src = `http://fanyi.youdao.com/openapi.do?keyfrom=engcard&key=1604033298&type=data&doctype=jsonp&callback=show&version=1.1&q=${text}`;
+
+  script.onload = function(e) {
+    // 加载成功
+    script.parentNode.removeChild(script);
+  };
+  script.onerror = function(e) {
+    // 加载失败
+    script.parentNode.removeChild(script);
+  };
+
+  document.body.appendChild(script);
+}
+
+/**
+ * jsonp数据返回
+ */
+function show(ret) {
+  if(ret.errorCode) {
+    // 异常
+    dispatch('error', ret.errorCode);
+  } else {
+    ret.hists = hists;
+    dispatch('success', ret);
+  }
+}
+
+/**
+ * 监听某个事件
+ */
+function listen(event, handel) {
+  handels[event] = handels[event] || [];
+  handels[event].push(handel);
+}
+
+/**
+ * 触发某个事件
+ */
+function dispatch(event, data) {
+  if(handels[event]) {
+    handels[event].forEach((handel) => {
+      handel(data);
+    });
+  }
+}
+
+/**
+ * 增加历史记录
+ */
+function addHist(text) {
+  text = text.trim();
+
+  for(let i=hists.length-1; i>=0 ;i--) {
+    if(hists[i] === text) hists.splice(i, 1);
+  }
+
+  while(hists.length >= 20) hists.unshift();
+  hists.push(text);
+}
+
+
 /**
  * 输入框组件
  */
 const Ipts = React.createClass({
+  search: function() {
+    let query = this.refs.searchIpt.value;
+    send(query);
+  },
   render: function() {
     return (
       <div className="ipt_box">
-				<input className="ipt" type="text" placeholder="请输入您要查询的单词或语句" />
-				<button className="btn" type="button"><i className="icon-search"></i></button>
+				<input className="ipt" type="text" ref="searchIpt" placeholder="请输入您要查询的单词或语句" />
+				<button className="btn" type="button" onClick={this.search}><i className="icon-search"></i></button>
 			</div>
     );
   }
@@ -16,25 +134,13 @@ const Ipts = React.createClass({
  * 翻译组件
  */
 const Trans = React.createClass({
-	getInitialState: function() {
-    return {trans: [
-    	'今天天气不错',
-    	'今天天气不错',
-    	'今天天气不错',
-    	'今天天气不错',
-    	'今天天气不错',
-    	'今天天气不错',
-    	'今天天气不错',
-    	'今天天气不错',
-    	'今天天气不错'
-    ]};
-  },
   render: function() {
+    let translation = this.props.translation;
     return (
       <div className="trans_box">
       	{
-      		this.state.trans.map((tran, index) => {
-      			return (<div className={'trans trans_' + index}>{tran}</div>);
+      		translation.map((tran, index) => {
+      			return (<div key={index} className={'trans trans_' + index%5}>{tran}</div>);
       		})
       	}
 			</div>
@@ -46,38 +152,30 @@ const Trans = React.createClass({
  * 释义组件
  */
 const Exps = React.createClass({
-	getInitialState: function() {
-    return {
-    	query: 'test',
-    	phonetic: 'yahei, xx',
-    	ukPhonetic: 'sss',
-    	usPhonetic: 'fff',
-    	exps: [
-    		'cc',
-    		'ss',
-    		'gg'
-    	]
-    };
-  },
   render: function() {
+    let query = this.props.query;
+    let phonetic = this.props.basic && this.props.basic.phonetic;
+    let ukPhonetic = this.props.basic && this.props.basic['uk-phonetic'];
+    let usPhonetic = this.props.basic && this.props.basic['us-phonetic'];
+    let explains = this.props.basic && this.props.basic.explains || [];
     return (
       <div className="exps_box">
-				<div className="query">{this.state.query}</div>
+				<div className="query">{query}</div>
 				{
-					!this.state.phonetic && !this.state.ukPhonetic && !this.usPhonetic ? '' : 
+					!phonetic && !ukPhonetic && !usPhonetic ? '' : 
 						<div className="pron">
-							{this.state.phonetic ? <div className="pron_item">[{this.state.phonetic}]</div> : ''}
-							{this.state.ukPhonetic ? <div className="pron_item">英[{this.state.ukPhonetic}]</div> : ''}
-							{this.state.usPhonetic ? <div className="pron_item">美[{this.state.usPhonetic}]</div> : ''}
+							{phonetic ? <div className="pron_item">[{phonetic}]</div> : ''}
+							{ukPhonetic ? <div className="pron_item">英[{ukPhonetic}]</div> : ''}
+							{usPhonetic ? <div className="pron_item">美[{tusPhonetic}]</div> : ''}
 						</div>
 				}
 				<div className="exps_title">释义 :</div>
 				{
-					!this.state.exps.length ? '' :
+					!explains.length ? '' :
 						<ul className="exps">
 							{
-								this.state.exps.map((exp) => {
-									return (<li className="exp">{exp}</li>);
+								explains.map((exp, index) => {
+									return (<li key={index} className="exp">{exp}</li>);
 								})
 							}
 						</ul>
@@ -91,21 +189,15 @@ const Exps = React.createClass({
  * 网络释义组件
  */
 const Nets = React.createClass({
-	getInitialState: function() {
-    return {nets: [
-    	{key: 'xxx', value: 'tttt'},
-    	{key: 'xx阿斯x', value: 'ttt阿斯达斯的t'},
-    	{key: 'xxx', value: 'tttt'}
-    ]};
-  },
   render: function() {
+    let web = this.props.web;
     return (
       <div className="nets_box">
 				<div className="nets_title">网络释义 :</div>
 				<ul className="nets">
 					{
-						this.state.nets.map((net) => {
-							return (<li className="net"><span className="net_key">{net.key}</span>:<span className="net_val">{net.value}</span></li>);
+						web.map((net, index) => {
+							return (<li key={index} className="net"><span className="net_key">{net.key}</span>:<span className="net_val">{net.value.join(' , ')}</span></li>);
 						})
 					}
 				</ul>
@@ -118,22 +210,15 @@ const Nets = React.createClass({
  * 历史记录组件
  */
 const Hists = React.createClass({
-	getInitialState: function() {
-    return {hists: [
-    	'xxxxx',
-    	'xxxxx',
-    	'xxxxx',
-    	'xxxxx'
-    ]};
-  },
   render: function() {
+    let hists = this.props.hists;
     return (
       <div className="hists_box">
 				<ul className="hists">
 					{
-						this.state.hists.map((hist) => {
+						hists.map((hist, index) => {
 							return (
-								<li className="hist">
+								<li key={index} className="hist">
 									<i className="icon-search"></i>
 									<a href="#" title={hist}>{hist}</a>
 								</li>
@@ -146,25 +231,20 @@ const Hists = React.createClass({
   }
 });
 
-var data = {
-	'query': 'test',
-	'basic': {
-		'phonetic': 'yahei, xx',
-		'uk-phonetic': 'sss',
-		'us-phonetic': 'fff',
-  	'explains': [
-  		'cc',
-  		'ss',
-  		'gg'
-  	]
-	}
-};
-
 /**
  * 整体布局组件
  */
 const Layouts = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  componentDidMount: function() {
+    listen('success', (data) => {
+      this.setState(data);
+    });
+  },
   render: function() {
+    let {basic, web, hists, translation, query} = this.state;
     return (
     	<div>
 	      <div className="top">
@@ -172,15 +252,15 @@ const Layouts = React.createClass({
 				</div>
 				<div className="bottom">
 					<div className="top_block_cnt">
-						<Trans />
+						{translation && translation.length ? <Trans translation={translation} /> : ''}
 					</div>
 					<div className="bottom_block_cnt">
 						<div className="left_cnt">
-							{data.basic ? <Exps basic={data.basic} /> : ''}
-							<Nets />
+							{basic ? <Exps basic={basic} query={query} /> : ''}
+							{web ? <Nets web={web} /> : ''}
 						</div>
 						<div className="right_cnt">
-							<Hists />
+							{hists && hists.length ? <Hists hists={hists} /> : ''}
 						</div>
 					</div>
 				</div>
